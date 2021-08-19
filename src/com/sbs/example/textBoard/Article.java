a58a2430f2c51b504286d8714bd8face4ee3038d
wjddls1916 <wjddls526526@naver.com>
2021-08-19 12:23:47 +0900

    첫 작업

---
+++ src/com/sbs/example/textBoard/Article.java
@@ -0,0 +1,10 @@
+package com.sbs.example.textBoard;
+
+public class Article {
+	public Article (int id, String title, String body2) {
+	}
+	
+		public int id;
+		public String title;
+		public String body;
+}
