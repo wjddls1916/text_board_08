e57ca9ad5329f064a8c752818391f25d6425fa02
wjddls1916 <80658464+wjddls1916@users.noreply.github.com>
2021-08-20 12:01:23 +0900

    JDBCInserTest

--- src/com/sbs/example/textBoard/Article.java
+++ src/com/sbs/example/textBoard/Article.java
@@ -1,10 +1,24 @@
 package com.sbs.example.textBoard;
 
-public class Article {
-	public Article (int id, String title, String body2) {
+public class Article extends Object {
+
+	public int id;
+	public String title;
+	public String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
-	
-		public int id;
-		public String title;
-		public String body;
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
+
 }
+
+
+
+
