9a977debf45ee1cad9333156cda4325cc2226c27
wjddls1916 <80658464+wjddls1916@users.noreply.github.com>
2021-08-23 12:14:12 +0900

    article list 명령시에 데이터를 DB에서 가져오기

---
+++ src/com/sbs/example/textBoard/test/JDBCConnectionTest.java
@@ -0,0 +1,10 @@
+package com.sbs.example.textBoard.test;
+
+public class JDBCConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/sbs/example/textBoard/test/JDBCSelectTest.java
@@ -0,0 +1,10 @@
+package com.sbs.example.textBoard.test;
+
+public class JDBCSelectTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
